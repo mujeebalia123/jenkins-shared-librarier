3ef4f4a5a319762849bec18206ec375e3a260af2
mujeebalia123 <130073399+mujeebalia123@users.noreply.github.com>
2026-01-18 17:41:41 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
-def call(String image, String tag) {
-    sh "docker build -t ${image}:${tag} ."
+// Define function
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
